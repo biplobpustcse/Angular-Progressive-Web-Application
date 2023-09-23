bfadb015f6256f9fae31f0303670823575813b43
biplob <biplobpust10@gmail.com>
2023-09-23 23:34:38 +0600

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isOnline: boolean;
+  constructor() {
+    this.isOnline = false;
+  }
+  ngOnInit(): void {
+    this.updateOnlineStatus();
+    window.addEventListener('online', this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+  }
+
+  private updateOnlineStatus(): void {
+    this.isOnline = window.navigator.onLine;
+  }
+
   title = 'angular-pwa';
 }
